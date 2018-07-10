13891f449348a9e3c30b851a989643757ef004fe
Leonardo Alvarez <leonardo.luis.alvarez@gmail.com>
2018-07-10 20:19:38 -0300

    Agregue codigo en el main

--- app/src/main/java/com/leonardo/ejemplo_android/MainActivity.java
+++ app/src/main/java/com/leonardo/ejemplo_android/MainActivity.java
@@ -2,12 +2,23 @@ package com.leonardo.ejemplo_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHolaMundo;
+    private String textoaMostrar = "hola mundo";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHolaMundo = findViewById(R.id.txtHolaMundo);
+
+       txtHolaMundo.setText(textoaMostrar);
+
+
     }
 }
